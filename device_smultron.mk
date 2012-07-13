ed72b2631592587ce17d06b40651534e2cc3af6a
freexperia <jerpelea@gmail.com>
2012-07-13 20:53:14 +0300

    smultron: update inclusion

--- device_smultron.mk
+++ device_smultron.mk
@@ -1,17 +1,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# common msm7x30 configs
+$(call inherit-product, device/semc/msm7x30-common/msm7x30.mk)
+
+# common mogami configs
+$(call inherit-product, device/semc/mogami-common/mogami.mk)
+
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
 # proprietary side of the device
 $(call inherit-product-if-exists, vendor/semc/smultron/smultron-vendor.mk)
-
-
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := smultron
-PRODUCT_DEVICE := smultron
-PRODUCT_MODEL := smultron
-
--include device/semc/mogami-common/mogami.mk
 
 # These is the hardware-specific overlay, which points to the location
 # of hardware-specific resource overrides, typically the frameworks and
@@ -32,12 +30,13 @@ PRODUCT_COPY_FILES += \
     device/semc/smultron/recovery.fstab:root/recovery.fstab
 
 
-# semc msm7x30 uses high-density artwork where available
-PRODUCT_LOCALES += mdpi
-
--include device/semc/msm7x30-common/prebuilt/resources-mdpi.mk
+$(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-mdpi.mk)
 
 #LCD DENSITY
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160 \
     com.qc.hdmi_out=false
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
