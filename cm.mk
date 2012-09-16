f6fd4e2359841bd389aaad12e1a4709c59225da6
freexperia <jerpelea@gmail.com>
2012-09-16 17:38:13 +0300

    smultron: fix device name for app compatibility check

--- cm.mk
+++ cm.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_RELEASE_NAME := ST15i
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST15i BUILD_FINGERPRINT="SEMC/ST15i_1249-8388/ST15i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="ST15i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND="Sony Ericsson" PRODUCT_NAME="Xperia mini" BUILD_FINGERPRINT="SEMC/ST15i_1249-8388/ST15i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="ST15i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-320x480
 
