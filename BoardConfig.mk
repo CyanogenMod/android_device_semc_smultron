c0b913b2d20011c195ef83895542bf41b1bb87bf
Michael Bestas <mikeioannina@gmail.com>
2013-03-08 03:41:49 +0200

    smultron: some cleanup
    * Removed Phone overlay (mdpi fix is merged in Phone app)
    * Added & fixed some comments to the source files
    * Reorganized alphabetically proprietary files list
    * recovery.fstab: convert tabs to spaces
    * Move gsm feature xml to mogami-common
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I80d21094c9138a95a61224a62cc04088d86c3b79

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,15 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/smultron/BoardConfigVendor.mk
 
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_smultron_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Recovery
 TARGET_RECOVERY_INITRC := device/semc/smultron/recovery/init.rc
 
+# Sensors
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := false
 SENSORS_ACCEL_BMA250_INPUT := true
